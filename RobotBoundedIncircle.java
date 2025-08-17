5da5a40a492f4e3aedc61c479eeddce8c5d4e836
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-08-17 12:34:26 -0500

    Create RobotBoundedIncircle.java

---
+++ RobotBoundedIncircle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+                                    //N, W, S, E
+        int[][] dirs = new int[][]{{0, 1}, {-1, 0}, { 0,-1}, {1, 0}};
+
+        int x = 0, y = 0;
+        int idx = 0;
+
+        for(int i=0; i<4; i++){
+            for(char c: instructions.toCharArray()){
+                if(c == 'G'){
+                    x += dirs[idx][0];
+                    y += dirs[idx][1];
+                }else if(c == 'L'){
+                    idx = (idx + 1 ) % 4;
+                }else if( c == 'R'){
+                    idx = (idx + 3) % 4;
+                }
+            }
+
+            if(x == 0 && y == 0) return true;
+        }
+
+        return false;
+    }
